e516194832a7f27def6186235106fa690dd20053
jdhungana01 <jasondhungana@pursuit.org>
2023-12-06 18:54:50 -0500

    finish exercise:

---
+++ script.js
@@ -0,0 +1,16 @@
+const greeting = 'Hello, world!';
+console.log (greeting);
+
+
+const number = 1
+const anotherNumber = 2;
+const sum = number + anotherNumber;
+console.log(sum);
+
+
+const name = 'Alice';
+if (name === 'Alice') {
+  console.log('Hello Alice!');
+} else {
+  console.log('You are not Alice');
+}
